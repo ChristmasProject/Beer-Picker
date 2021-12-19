2bd7d21bb705fdbb5981ffe6c69126baf17e13f3
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-18 21:53:47 -0600

    fixed missing quotation

--- js/beer-picker.js
+++ js/beer-picker.js
@@ -1,31 +1,31 @@
-var image1 = <img src='../img'/>
-var image2 = <img src='../img'/>
-var image3 = <img src='../img'/>
-var image4 = <img src='../img'/>
+const image1 = <img src='../img/light-beers.webp' alt='light beer'/>
+const image2 = <img src='../img'/>
+const image3 = <img src='../img'/>
+const image4 = <img src='../img'/>
 
-var beers = [
-    {id: 1, name: "Nite Lite", style: "light", image1},
-    {id: 2, name: "Slightly Mighty", style: "light", image1},
-    {id: 3, name: "Bell’s Light Hearted", style: "light", image1},
-    {id: 4, name: "Harpoon Rec.", style: "light", image1},
-    {id: 5, name: "Kona Blond", style: "light", image1},
-    {id: 6, name: "Omission Ultimate", style: "light", image1},
-    {id: 7, name: "Polygamy Nitro", style: "dark", image2},
-    {id: 8, name: "Cutthroat Porter", style: "dark", image2},
-    {id: 9, name: "Old Fezziwig", style: "dark", image2},
-    {id: 10, name: "Edmund Fitzgerald", style: "dark", image2},
-    {id: 11, name: "La Trappe Dubbel", style: "dark", image2},
-    {id: 12, name: "Shark Attack", style: "dark", image2},
-    {id: 13, name: "Devils Backbone", style: "cider", image3},
-    {id: 14, name: "Dooryard", style: "cider", image3},
-    {id: 15, name: "La Mûre", style: "cider", image3},
-    {id: 16, name: "Woodchuck", style: "cider", image3},
-    {id: 17, name: "Pommeau", style: "cider", image3},
-    {id: 18, name: "Blueberry Spaceship", style: "cider", image3},
-    {id: 19, name: "Keller Pils, style: "amber", image4},
-    {id: 20, name: "Alt-Eration", style: "amber", image4},
-    {id: 21, name: "Balt Altbier", style: "amber", image4},
-    {id: 22, name: "Double Nickel", style: "amber", image4},
-    {id: 23, name: "Free Time", style: "amber", image4},
-    {id: 24, name: "Grevensteiner", style: "amber", image4},
+let beers = [
+    {id: 1, name: "Nite Lite", style: "light", image: image1},
+    {id: 2, name: "Slightly Mighty", style: "light", image: image1},
+    {id: 3, name: "Bell’s Light Hearted", style: "light", image: image1},
+    {id: 4, name: "Harpoon Rec.", style: "light", image: image1},
+    {id: 5, name: "Kona Blond", style: "light", image: image1},
+    {id: 6, name: "Omission Ultimate", style: "light", image: image1},
+    {id: 7, name: "Polygamy Nitro", style: "dark", image: image2},
+    {id: 8, name: "Cutthroat Porter", style: "dark", image: image2},
+    {id: 9, name: "Old Fezziwig", style: "dark", image: image2},
+    {id: 10, name: "Edmund Fitzgerald", style: "dark", image: image2},
+    {id: 11, name: "La Trappe Dubbel", style: "dark", image: image2},
+    {id: 12, name: "Shark Attack", style: "dark", image: image2},
+    {id: 13, name: "Devils Backbone", style: "cider", image: image3},
+    {id: 14, name: "Dooryard", style: "cider", image: image3},
+    {id: 15, name: "La Mûre", style: "cider", image: image3},
+    {id: 16, name: "Woodchuck", style: "cider", image: image3},
+    {id: 17, name: "Pommeau", style: "cider",image: image3},
+    {id: 18, name: "Blueberry Spaceship", style: "cider", image: image3},
+    {id: 19, name: "Keller Pils", style: "amber", image: image4},
+    {id: 20, name: "Alt-Eration", style: "amber", image: image4},
+    {id: 21, name: "Balt Altbier", style: "amber", image: image4},
+    {id: 22, name: "Double Nickel", style: "amber", image: image4},
+    {id: 23, name: "Free Time", style: "amber", image: image4},
+    {id: 24, name: "Grevensteiner", style: "amber", image: image4},
 ]
