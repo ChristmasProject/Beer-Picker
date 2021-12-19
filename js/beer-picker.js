c2186e52403c3c797a28deac048bcb36041bb8f8
Cody Hodges <hodges.cody00@gmail.com>
2021-12-19 12:40:24 -0600

    Push to main

--- js/beer-picker.js
+++ js/beer-picker.js
@@ -1,30 +1,30 @@
 "use strict";
 
 let beers = [
-    {id: 1, name: "Nite Lite", style: "light", image: "/img/light-beers.webp"},
-    {id: 2, name: "Slightly Mighty", style: "light", image: "/img/light-beers.webp"},
-    {id: 3, name: "Bell’s Light Hearted", style: "light", image: "/img/light-beers.webp"},
-    {id: 4, name: "Harpoon Rec.", style: "light", image: "/img/light-beers.webp"},
-    {id: 5, name: "Kona Blond", style: "light", image: "/img/light-beers.webp"},
-    {id: 6, name: "Omission Ultimate", style: "light", image: "/img/light-beers.webp"},
-    {id: 7, name: "Polygamy Nitro", style: "dark", image: "/img/light-beers.webp"},
-    {id: 8, name: "Cutthroat Porter", style: "dark", image: "/img/light-beers.webp"},
-    {id: 9, name: "Old Fezziwig", style: "dark", image: "/img/light-beers.webp"},
-    {id: 10, name: "Edmund Fitzgerald", style: "dark", image: "/img/light-beers.webp"},
-    {id: 11, name: "La Trappe Dubbel", style: "dark", image: "/img/light-beers.webp"},
-    {id: 12, name: "Shark Attack", style: "dark", image: "/img/light-beers.webp"},
-    {id: 13, name: "Devils Backbone", style: "cider", image: "/img/light-beers.webp"},
-    {id: 14, name: "Dooryard", style: "cider", image: "/img/light-beers.webp"},
-    {id: 15, name: "La Mûre", style: "cider", image: "/img/light-beers.webp"},
-    {id: 16, name: "Woodchuck", style: "cider", image: "/img/light-beers.webp"},
-    {id: 17, name: "Pommeau", style: "cider",image: "/img/light-beers.webp"},
-    {id: 18, name: "Blueberry Spaceship", style: "cider", image: "/img/light-beers.webp"},
-    {id: 19, name: "Keller Pils", style: "amber", image: "/img/light-beers.webp"},
-    {id: 20, name: "Alt-Eration", style: "amber", image: "/img/light-beers.webp"},
-    {id: 21, name: "Balt Altbier", style: "amber", image: "/img/light-beers.webp"},
-    {id: 22, name: "Double Nickel", style: "amber", image: "/img/light-beers.webp"},
-    {id: 23, name: "Free Time", style: "amber", image: "/img/light-beers.webp"},
-    {id: 24, name: "Grevensteiner", style: "amber", image: "/img/light-beers.webp"},
+    {id: 1, name: "Nite Lite", style: "light", image: "/img/light.jpeg"},
+    {id: 2, name: "Slightly Mighty", style: "light", image: "/img/light.jpeg"},
+    {id: 3, name: "Bell’s Light", style: "light", image: "/img/light.jpeg"},
+    {id: 4, name: "Harpoon Rec.", style: "light", image: "/img/light.jpeg"},
+    {id: 5, name: "Kona Blond", style: "light", image: "/img/light.jpeg"},
+    {id: 6, name: "Omission Ultimate", style: "light", image: "/img/light.jpeg"},
+    {id: 7, name: "Polygamy Nitro", style: "dark", image: "/img/dark.jpeg"},
+    {id: 8, name: "Cutthroat Porter", style: "dark", image: "/img/dark.jpeg"},
+    {id: 9, name: "Old Fezziwig", style: "dark", image: "/img/dark.jpeg"},
+    {id: 10, name: "Edmund Fitzgerald", style: "dark", image: "/img/dark.jpeg"},
+    {id: 11, name: "La Trappe Dubbel", style: "dark", image: "/img/dark.jpeg"},
+    {id: 12, name: "Shark Attack", style: "dark", image: "/img/dark.jpeg"},
+    {id: 13, name: "Devils Backbone", style: "cider", image: "/img/cider.jpeg"},
+    {id: 14, name: "Dooryard", style: "cider", image: "/img/cider.jpeg"},
+    {id: 15, name: "La Mûre", style: "cider", image: "/img/cider.jpeg"},
+    {id: 16, name: "Woodchuck", style: "cider", image: "/img/cider.jpeg"},
+    {id: 17, name: "Pommeau", style: "cider",image: "/img/cider.jpeg"},
+    {id: 18, name: "Blueberry Spaceship", style: "cider", image: "/img/cider.jpeg"},
+    {id: 19, name: "Keller Pils", style: "amber", image: "/img/amber.jpeg"},
+    {id: 20, name: "Alt-Eration", style: "amber", image: "/img/amber.jpeg"},
+    {id: 21, name: "Balt Altbier", style: "amber", image: "/img/amber.jpeg"},
+    {id: 22, name: "Double Nickel", style: "amber", image: "/img/amber.jpeg"},
+    {id: 23, name: "Free Time", style: "amber", image: "/img/amber.jpeg"},
+    {id: 24, name: "Grevensteiner", style: "amber", image: "/img/amber.jpeg"},
 ]
 
 // THIS FUNCTION WRITES EACH LINE OF HTML AND RETURNS IT TO THE LOOP FUNCTION
@@ -32,7 +32,7 @@ let beers = [
 function createHTML(beer){
 
     let html = '<div class= "col-md-4 col-lg-3 beers"><div><img src="';
-    html += beer.image + '"></div><div><h2>' + beer.name + '</h2></div><div><h3>' + beer.style + '</h3></div></div>';
+    html += beer.image + '"></div><div><h2>' + beer.name + '</h2></div><hr><div><h3>' + beer.style + '</h3></div></div>';
 
     return html;
 }
