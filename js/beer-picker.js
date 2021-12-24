9c112187516e0ce931fb15e07aaacf75a50d27c9
Nelson Delpozo <nelson.delpozo99@gmail.com>
2021-12-23 22:17:15 -0700

    working on that dropdown

--- js/beer-picker.js
+++ js/beer-picker.js
@@ -12,7 +12,7 @@ let beers = [
     {id: 8, name: "Dooryard", style: "cider", image: "./img/cider.jpeg"},
     {id: 9, name: "La Mûre", style: "cider", image: "./img/cider.jpeg"},
     {id: 10, name: "Woodchuck", style: "cider", image: "./img/cider.jpeg"},
-    {id: 11, name: "Pommeau", style: "cider",image: "./img/cider.jpeg"},
+    {id: 11, name: "Pommeau", style: "cider", image: "./img/cider.jpeg"},
     {id: 12, name: "Blueberry Spaceship", style: "cider", image: "./img/cider.jpeg"},
     {id: 13, name: "Sunset Skies", style: "amber", image: "./img/amber.jpeg"},
     {id: 14, name: "Alt-Eration", style: "amber", image: "./img/amber.jpeg"},
@@ -107,7 +107,7 @@ let supportersContent = `<div class="container">
 
 // THIS FUNCTION WRITES EACH LINE OF HTML AND RETURNS IT TO THE LOOP FUNCTION
 
-function createHTML(beer){
+function createHTML(beer) {
 
     let html = '<div class= "col-md-4 col-lg-3 beers"><div><img src="';
     html += beer.image + '"></div><div><h2>' + beer.name + '</h2></div><hr><div><h3>' + beer.style + '</h3></div></div>';
@@ -118,9 +118,9 @@ function createHTML(beer){
 // THIS LOOP FUNCTION RUNS EACH BEER THROUGH THE FIRST FUNCTION ABOVE AND RETURNS THE HTML
 
 
-function createColumns(beers){
+function createColumns(beers) {
     let html = '';
-    for(let i = 0; i < beers.length; i++){
+    for (let i = 0; i < beers.length; i++) {
         html += createHTML(beers[i]);
     }
     return html;
@@ -133,26 +133,26 @@ function updateBeers(e) {
     let selectedBeer = beerSelection.value;
     let filteredBeers = [];
 
-    if(beerSelection.value === 'all'){
+    if (beerSelection.value === 'all') {
         contentArea.innerHTML = createColumns(beers);
         return;
-    }else
-    beers.forEach(function(beer) {
-        if (beer.style === selectedBeer) {
-            filteredBeers.push(beer);
-        }
-    });
+    } else
+        beers.forEach(function (beer) {
+            if (beer.style === selectedBeer) {
+                filteredBeers.push(beer);
+            }
+        });
     contentArea.innerHTML = createColumns(filteredBeers);
 }
 
 // THIS IS FOR WHEN YOU SELECT A BEER IN THE TEXT INPUT
 
-function selectBeer(e){
+function selectBeer(e) {
     e.preventDefault();
     let chosenBeer = beerNameSelection.value;
     let beerChoice = [];
-    beers.forEach(function(beer){
-        if (beer.name.toLowerCase().replace(/\s+/g, '') === chosenBeer.toLowerCase().replace(/\s+/g, '')){
+    beers.forEach(function (beer) {
+        if (beer.name.toLowerCase().replace(/\s+/g, '') === chosenBeer.toLowerCase().replace(/\s+/g, '')) {
             beerChoice.push(beer);
         }
     });
@@ -170,30 +170,30 @@ function newBeer(e) {
     newBeerSubmission.name = newSubmissionName;
     newBeerSubmission.style = newSubmissionStyle;
 // THIS MAKES SURE THERE'S A NAME ENTERED'
-    if(newSubmissionName === ''){
+    if (newSubmissionName === '') {
         dupeAlert.innerHTML = "It's gotta have a name dude"
         return;
     }
 // THIS MAKES SURE IT DOESN'T ALREADY EXIST'
-    for(let i = 0; i < beers.length; i++){
-        if(newBeerName.value.toLowerCase().replace(/\s+/g, '') === beers[i].name.toLowerCase().replace(/\s+/g, '')){
-        dupeAlert.innerHTML = "That beer already exists bud";
-        return;
+    for (let i = 0; i < beers.length; i++) {
+        if (newBeerName.value.toLowerCase().replace(/\s+/g, '') === beers[i].name.toLowerCase().replace(/\s+/g, '')) {
+            dupeAlert.innerHTML = "That beer already exists bud";
+            return;
 
 // THIS ADDS IMAGE TO THE NEW BEER OBJECT
-        }else{
+        } else {
             newBeerSubmission.name = newSubmissionName;
-        newBeerSubmission.style = newSubmissionStyle;
-        dupeAlert.innerHTML = '';
-        if (newSubmissionStyle === 'light') {
-            newBeerSubmission.image = "./img/light.jpeg";
-        } else if (newSubmissionStyle === 'amber') {
-            newBeerSubmission.image = './img/amber.jpeg'
-        } else if (newSubmissionStyle === 'dark') {
-            newBeerSubmission.image = './img/dark.jpeg';
-        } else
-            newBeerSubmission.image = './img/cider.jpeg';
-    }
+            newBeerSubmission.style = newSubmissionStyle;
+            dupeAlert.innerHTML = '';
+            if (newSubmissionStyle === 'light') {
+                newBeerSubmission.image = "./img/light.jpeg";
+            } else if (newSubmissionStyle === 'amber') {
+                newBeerSubmission.image = './img/amber.jpeg'
+            } else if (newSubmissionStyle === 'dark') {
+                newBeerSubmission.image = './img/dark.jpeg';
+            } else
+                newBeerSubmission.image = './img/cider.jpeg';
+        }
     }
     beers.push(newBeerSubmission);
     console.log(beers);
@@ -201,7 +201,7 @@ function newBeer(e) {
 }
 
 // THIS FUNCTION SHOWS THE ABOUT US CONTENT AS WELL AS THE RETURN HOME BUTTON, WHICH RELOADS THE PAGE
-function showAbout(){
+function showAbout() {
     aboutPage.innerHTML = aboutUsContent;
     homeBTN.innerHTML = `<a href="#" onclick="location.reload()">Return Home</a>`
 }
@@ -221,6 +221,7 @@ let contentArea = document.querySelector('.row');
 // THESE ARE THE DROPDOWNS AND INPUT FIELDS
 let submitButton = document.querySelector('#selection');
 let beerSelection = document.querySelector('#sel1');
+// beerSelection.addEventListener('change', selectBeer);
 let newBeerName = document.querySelector('#added-beer-name');
 let newBeerStyle = document.querySelector('#added-beer-style');
 
